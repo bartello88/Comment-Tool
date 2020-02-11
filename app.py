be5e09ea304f340cc88d43d28260143eb0595218
bartello88 <bartello88@gmail.com>
2020-02-11 21:24:27 +0100

    Working with re

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, flash, redirect, jsonify
 import psycopg2
+from pattern import check_pattern
 #--------------------------------------------------------
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.ext.automap import automap_base
@@ -15,6 +16,7 @@ app.secret_key = 'don tell anybody'
 #--------------------------------------------------------
 # connection = get_database_credentials_from_YAML()
 # database_uri = f"{connection['database']}+{connection['database_dialect']}://{connection['user']}:{connection['password']}@{connection['host']}/{connection['database_name']}"
+
 
 # print(database_uri)
 #
@@ -37,12 +39,6 @@ app.secret_key = 'don tell anybody'
 # print(Sessions)
 #--------------------------------------------------------
 
-try:
-    connection_string= "dbname='statistics' user='sxd' host='plsrvup-sxd01.ttg.global' password='sxd'"
-    conn = psycopg2.connect(connection_string)
-    print("Database connected")
-except Exception as e:
-    print(e)
 
 # Getting current list of sessions
 def prepareSessions(sessions_without_quote):
@@ -71,6 +67,12 @@ def addComment():
     if request.method == 'POST':
         if 'comment' in request.form:
             if request.form['text']:
+                try:
+                    connection_string = "dbname='statistics' user='sxd' host='plsrvup-sxd01.ttg.global' password='sxd'"
+                    conn = psycopg2.connect(connection_string)
+                    print("Database connected")
+                except Exception as e:
+                    print(e)
                 list_of_sessions = request.form['text']
                 comment_to_selects = request.form['option']
                 sessions_without_quote = list_of_sessions.replace("'", '').replace('\r\n', '\n').replace('"', '')
@@ -78,9 +80,10 @@ def addComment():
                 correct_sessions = [session.strip() for session in result if session]
                 # update session's comment
                 cur = conn.cursor()
-                wrong_sessions = []
                 sessions_exists_in_database = []
-                for session in correct_sessions:
+                sessions = check_pattern(correct_sessions)
+                wrongs = list(set(correct_sessions).difference(set(sessions)))
+                for session in sessions:
                     cur.execute(f"""SELECT * FROM moma_reporting.comments WHERE "sessionname" LIKE '{session}'""")
                     result= cur.fetchone()
                     if result is not None:
@@ -88,12 +91,14 @@ def addComment():
                             f"""UPDATE moma_reporting.comments SET comment = '{comment_to_selects}' WHERE sessionname like '{session}'""")
                         conn.commit()
                         sessions_exists_in_database.append(session)
-                    else:
-                        print(f"Wrong session {session}")
-                        wrong_sessions.append(session)
+                    elif result is None:
+                        cur.execute(
+                            f"""INSERT INTO moma_reporting.comments VALUES ('{session}','{comment_to_selects}')""")
+                        conn.commit()
+                        sessions_exists_in_database.append(session)
                 cur.close()
                 good_sessions_len = len(sessions_exists_in_database)
-                if len(wrong_sessions) > 0:
+                if len(wrongs) > 0:
                     flash(
                         f"You have just commented {good_sessions_len} sessions and have {len(wrong_sessions)} issue(s)",
                         'warning')
@@ -103,7 +108,7 @@ def addComment():
                 flash("You didn't provide sessions amigo", 'error')
                 return render_template('index.html')
         return render_template('index.html', sessions_exists_in_database=sessions_exists_in_database,
-                               wrong_sessions=wrong_sessions, option=comment_to_selects)
+                               wrongs=wrongs, option=comment_to_selects)
 
 
 if __name__ == '__name__':
